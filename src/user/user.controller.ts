28ffc71a1298fe883ea336d700686861d929208c
Felipe <felipegraca.1999@gmail.com>
2023-08-11 01:56:56 -0300

    Add bcrypt package and create feature to crypt user password

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create_user.dto';
 import { UserService } from './user.service';
+import { User } from './interfaces/user.interface';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  async createUser(
-    @Body() createUserDTO: CreateUserDTO,
-  ): Promise<CreateUserDTO> {
+  async createUser(@Body() createUserDTO: CreateUserDTO): Promise<User> {
     return this.userService.createUser(createUserDTO);
+  }
+
+  @Get()
+  async getAllUsers(): Promise<User[]> {
+    return this.userService.getAllUsers();
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create_user.dto';
 import { User } from './interfaces/user.interface';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
   private users: User[] = [];
 
-  async createUser(createUserDTO: CreateUserDTO): Promise<User> {
-    const user: User = {
-      ...createUserDTO,
-      id: this.users.length + 1,
-    };
+  constructor() {}
 
+  async createUser(createUserDTO: CreateUserDTO): Promise<User> {
+    const saltDrRound = 10;
+    const hashPassword = await hash(createUserDTO.password, saltDrRound);
+
+    const user: User = {
+      id: this.users.length + 1,
+      ...createUserDTO,
+      password: hashPassword,
+    };
     this.users.push(user);
 
     return user;
+  }
+
+  async getAllUsers(): Promise<User[]> {
+    return this.users;
   }
 }
